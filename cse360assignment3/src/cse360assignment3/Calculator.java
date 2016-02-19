6355772c3f00ad423c14dfe5a5e7c5b13979b8ad
Justin Sanchez <jdsanch8@asu.edu>
2016-02-19 10:02:07 -0700

    Inital check in of Calculator

---
+++ cse360assignment3/src/cse360assignment3/Calculator.java
@@ -0,0 +1,67 @@
+/**
+ * This class contains an int that can be added, subtracted, multiplied, and divided to.
+ * @author Justin Sanchez, PIN# 802
+ * @version Feb 19, 2016
+ */
+
+package cse360assignment3;
+
+public class Calculator {
+
+	private int total;
+	
+	/**
+	 * Constructor that sets total to be 0.
+	 */
+	public Calculator () {
+		total = 0;  // not needed - included for clarity
+	}
+	
+	/**
+	 * Gets the value of the int total
+	 * @return the value for total
+	 */
+	public int getTotal () {
+		return 0;
+	}
+	
+	/**
+	 * Adds a value to the total
+	 * @param value number being added to total
+	 */
+	public void add (int value) {
+		
+	}
+	
+	/**
+	 * Subtracts a value from the total
+	 * @param value number being subtracted from total
+	 */
+	public void subtract (int value) {
+		
+	}
+	
+	/**
+	 * Multiplies a the total by the number
+	 * @param value number to be multiplied by
+	 */
+	public void multiply (int value) {
+		
+	}
+	
+	/**
+	 * Divides the total by the number
+	 * @param value number to be divided by
+	 */
+	public void divide (int value) {
+		
+	}
+	
+	/**
+	 * The previous operations that have been performed
+	 * @return String containing previous operations
+	 */
+	public String getHistory () {
+		return "";
+	}
+}
